617cf8b9a2da11d58d4234f8d9b28354c049a2cc
Pobitrodeb <pobitrodn@gmail.com>
2023-02-22 23:40:20 +0600

    call vuex

--- src/components/store.js
+++ src/components/store.js
@@ -2,3 +2,17 @@ import Vue from "vue"
 import Vuex from "vuex"
 
 Vue.use(Vuex)
+
+
+new Vue({ 
+    data() { 
+        return { 
+            
+            } 
+    }, 
+})
+
+new .Vuex.store({
+	state:{
+	}
+})
