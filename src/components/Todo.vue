b5e78b35f462b6e6950c5901ad05aa0ee48a369a
Pobitrodeb <pobitrodn@gmail.com>
2023-02-22 23:28:26 +0600

    todo componnents added

---
+++ src/components/Todo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+       <Todo/>
+    </div>
+</template>
+
+<script>
+import Todo from "./components/Todo.vue";
+
+export default {
+    components:{Todo}
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
